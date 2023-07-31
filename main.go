478549147a669b75536774935b2dd1c57d661f84
Enrique M. Talavera <kiquetal@gmail.com>
2023-07-31 09:36:37 -0400

    adding files

--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 
-	fmt.Print("Hello World")
+	//fmt.Print("Hello World")
+
+	lines := strings.Split("aSDASDASD,badada asdsda ,csdasdasda asdasd asdasda ", ",")
+	for i, line := range lines {
+		fmt.Println(i, line)
+		wc := len(strings.Fields(line))
+		fmt.Println(wc)
+	}
 }
