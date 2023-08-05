3e0ec6f8d2420664579e0d487a1b6183c747b347
Enrique M. Talavera <kiquetal@gmail.com>
2023-08-04 23:03:23 -0400

    using testing package

--- stringutils/stringutils_test.go
+++ stringutils/stringutils_test.go
@@ -10,3 +10,22 @@ func TestUpper(t *testing.T) {
 		t.Error("Expected TEST, got ", upper)
 	}
 }
+
+func TestLower(t *testing.T) {
+
+	tests := map[string]struct {
+		input    string
+		expected string
+	}{
+		"basic": {input: "TEST", expected: "test"},
+		"empty": {input: "", expected: ""},
+	}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			lower := Lower(tc.input)
+			if lower != tc.expected {
+				t.Errorf("Expected %s, got %s", tc.expected, lower)
+			}
+		})
+	}
+}
