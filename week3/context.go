692980be9c936dafe8c48bc926d53b9f8cfd8370
Enrique M. Talavera <kiquetal@gmail.com>
2023-08-20 23:46:44 -0400

    timeout context

--- week3/context.go
+++ week3/context.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"time"
 )
 
 func main() {
-
+	http.HandleFunc("/", greetHandlerCtx)
+	log.Println("Server listening on :8080...")
+	log.Fatal(http.ListenAndServe("127.0.0.1:8080", nil))
 }
 
 func greetHandlerCtx(w http.ResponseWriter, r *http.Request) {
@@ -21,8 +24,15 @@ func greetHandlerCtx(w http.ResponseWriter, r *http.Request) {
 	for {
 		select {
 		case <-completeAfter:
-
+			fmt.Fprintln(w, "Hello Gopher!")
+			return
+		case <-ctx.Done():
+			err := ctx.Err()
+			log.Printf("Context Error: %s", err.Error())
+			return
+		default:
+			time.Sleep(1 * time.Second)
+			log.Println("Greetings are hard. Thinking...")
 		}
 	}
-
 }
